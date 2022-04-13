c613244b599ff46870516e19813f7ac35d94e1f4
trenev <trenevs@gmail.com>
2022-04-13 20:04:37 +0300

    Added custom form for user registration

--- bookbar/auth_app/forms.py
+++ bookbar/auth_app/forms.py
@@ -1,11 +1,20 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth import forms as auth_forms
+
+from bookbar.common.mixins import BootstrapFormControlMixin
 
 UserModel = get_user_model()
 
 
-class UserRegistrationForm(auth_forms.UserCreationForm):
+class UserRegistrationForm(BootstrapFormControlMixin, auth_forms.UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap_form_controls()
+        self.fields['password2'].help_text = None
+
     class Meta:
         model = UserModel
-        fields = ['email']
+        fields = ['email', ]
+
 
