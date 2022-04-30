679c3e0cdf431ec2ff2a6f48734945989ccf8cbe
trenev <trenevs@gmail.com>
2022-04-30 11:27:26 +0300

    Fixed cart template

--- tests/views/test_books_view.py
+++ tests/views/test_books_view.py
@@ -1,35 +1,34 @@
-# from datetime import date
-#
-# from django.test import TestCase
-# from django.urls import reverse
-#
-# from bookbar.books.models import Book, Category
-#
-#
-# class BooksViewTest(TestCase):
-#     VALID_CATEGORY = {
-#         'category_name': 'some category',
-#     }
-#
-#     VALID_BOOK_DATA = {
-#         'title': 'title',
-#         'author': 'author',
-#         'annotation': 'annotation',
-#         'cover_image': 'zaq.jpg',
-#         'quantity': 1,
-#         'price': 5,
-#         'date_added': date.today(),
-#     }
-#
-#     def test_books__when_no_specific_category(self):
-#         category = Category.objects.create(**self.VALID_CATEGORY)
-#         book = Book.objects.create(
-#             **self.VALID_BOOK_DATA,
-#             category=category,
-#         )
-#         response = self.client.get(reverse('show books', kwargs={
-#             'category': 'all',
-#         }))
-#         self.assertTemplateUsed(response, 'books/books.html')
-#
-#
+from datetime import date
+
+from django.test import TestCase
+from django.urls import reverse
+
+from bookbar.books.models import Book, Category
+
+
+class BooksViewTest(TestCase):
+    VALID_CATEGORY = {
+        'category_name': 'some category',
+    }
+
+    VALID_BOOK_DATA = {
+        'title': 'title',
+        'author': 'author',
+        'annotation': 'annotation',
+        'cover_image': 'zaq.jpg',
+        'quantity': 1,
+        'price': 5,
+        'date_added': date.today(),
+    }
+
+    def test_books__when_no_specific_category(self):
+        category = Category.objects.create(**self.VALID_CATEGORY)
+        book = Book.objects.create(
+            **self.VALID_BOOK_DATA,
+            category=category,
+        )
+        response = self.client.get(reverse('show books', kwargs={
+            'category': 'all',
+        }))
+        self.assertTemplateUsed(response, 'books/books.html')
+
